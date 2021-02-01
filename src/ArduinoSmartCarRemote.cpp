047a3114a572fc6016709d81d0a20c6e7baf5ddc
Eugen Zimmermann <EugenZimmermann@users.noreply.github.com>
2021-02-01 22:38:08 +0100

    Calibrated potentiometers and mapped to range of 0 - 1023, removed gyroscope

--- src/ArduinoSmartCarRemote.cpp
+++ src/ArduinoSmartCarRemote.cpp
@@ -21,7 +21,6 @@ void serialControl();
 
 void readButtons();
 void readJoysticks();
-void readGyroscopeSensor();
 /*********************************************************/
 
 /*
@@ -35,7 +34,6 @@ Scheduler taskManager;
 // sensor and calculations
 Task tButtons(updateInterval, TASK_FOREVER, &readButtons, &taskManager, true);
 Task tJoysticks(updateInterval, TASK_FOREVER, &readJoysticks, &taskManager, true);
-Task tGyroscope(updateInterval, TASK_FOREVER, &readGyroscopeSensor, &taskManager, false);
 
 // communication
 Task tRF(0, TASK_FOREVER, &comRF, &taskManager, true);
@@ -47,7 +45,6 @@ Task tSerialControl(updateIntervalSerial / 2, TASK_FOREVER, &serialControl, &tas
  * Drive mode and general variables
  ********************************************************/
 #define debug 0                 //#ToDo
-int inputType = 0;              //switch between joystick remote and steering wheel remote
 int mode = 1;
 bool automatic = false;
 /*********************************************************/
@@ -58,21 +55,21 @@ bool automatic = false;
 RF24 radio(9, 10);              // define the object to control NRF24L01
 byte addresses[][6] = {"00001","00007"};
 // new structure
-// data[0] = mode (1 manual joystick, 2 manual gyroscope, 3 automatic ultrasonic sensors, 4 automatic line follower)
-// data[1] = direction X of joystick J1 (not assigned yet) (used as return channel for xy)
-// data[2] = direction Y of joystick J1 (front/back speed) (used as return channel for speed)
-// data[3] = fine tuning joystick J1 (used as return channel xy)
-// data[4] = direction X of joystick J2 (left/right) (used as return channel for direction)
-// data[5] = direction Y of joystick J2 (front buzzer, back not assigned yet) (used as return channel for xy)
-// data[6] = fine tuning joystick J2 (used as return channel for xy)
-// data[7] = data return channel to remote (used as return channel for left distance)
-// data[8] = data return channel to remote (used as return channel for right distance)
-// data[9] = data return channel to remote (used as return channel for line follower sensor value)
+// data[apMode] = mode (1 manual joystick, 2 manual gyroscope, 3 automatic ultrasonic sensors, 4 automatic line follower)
+// data[apJ1x] = direction X of joystick J1 (not assigned yet) (used as return channel for xy)
+// data[apSpeed] = direction Y of joystick J1 (front = 1023/back = 0 speed) (used as return channel for speed)
+// data[apJ1fine] = fine tuning joystick J1 (used as return channel xy)
+// data[apDirection4] = direction X of joystick J2 (left = 1023/right = 0) (used as return channel for direction)
+// data[apJ2y] = direction Y of joystick J2 (front buzzer, back not assigned yet) (used as return channel for xy)
+// data[apJ2fine] = fine tuning joystick J2 (used as return channel for xy)
+// data[apUL] = data return channel to remote (used as return channel for left distance)
+// data[apUR] = data return channel to remote (used as return channel for right distance)
+// data[apLFS] = data return channel to remote (used as return channel for line follower sensor value)
 #define apMode 0
 #define apJ1x 1
-#define apJ1ySpeed 2
+#define apSpeed 2
 #define apJ1fine 3
-#define apJ2xDirection 4
+#define apDirection 4
 #define apJ2y 5
 #define apJ2fine 6
 #define apUL 7
@@ -91,26 +88,18 @@ bool bIsParameter;
 /*********************************************************/
 
 /*
- * Joysticks and buttons
+ * Potentiometers and buttons
  ********************************************************/
-const int pinJ1x = A5;                // define pin for direction X of joystick J1 (unused)
-const int pinJ1ySpeed = A1;           // define pin for direction Y of joystick J1 (speed)
-const int pinJ2xDirection = A2;       // define pin for direction X of joystick J2 (direction)
-const int pinJ2y = A3;                // define pin for direction Y of joystick J2 (horn)
-
-// Potentiometers for fine tuning the joysticks
-const int R1Pin = 6;            // define R1
-const int R6Pin = 7;            // define R6
+const int pinJ1ySpeed = A1;           // define pin for accelerator and brake pedal J1 (speed)
+const int pinJ2xDirection = A2;       // define pin for stearing wheel J2 (direction)
+const int pinGearshiftLeft = A6;      // define pin for gearshift left (unused)
+const int pinGearshiftRight = A7;     // define pin for gearshift right (horn)
 
 // Buttons
 const int APin = 2;             // define pin for D2
 const int BPin = 3;             // define pin for D3
 const int CPin = 4;             // define pin for D4
 const int DPin = 5;             // define pin for D5
-
-// Gearshift on steering wheel
-const int ShiftLPin = 0;        // define pin for D6
-const int ShiftRPin = 1;        // define pin for D7
 /*********************************************************/
 
 /*
@@ -121,70 +110,6 @@ const int led2Pin = 7;          // define pin for LED2 which is the mode is disp
 const int led3Pin = 8;          // define pin for LED3 which is the mode is displayed in the car auto mode
 /*********************************************************/
 
-/*
- * Gyroscope
- ********************************************************/
-#define DEVICE (0x53) //ADXL345 device address
-#define TO_READ (6)   //num of bytes we are going to read each time (two bytes for each axis)
-
-byte buff[TO_READ]; //6 bytes buffer for saving data read from the device
-char str[512];      //string buffer to transform data before sending it to the serial port
-
-int regAddress = 0x32;            //first axis-acceleration-data register on the ADXL345
-int x, y, z;                      //three axis acceleration data
-double roll = 0.00, pitch = 0.00; //Roll & Pitch are the angles which rotate by the axis X and y
-                                  //in the sequence of R(x-y-z)
-/*********************************************************/
-
-// ADXL345 Gyroscope Functions
-//Writes val to address register on device
-void writeTo(int device, byte address, byte val) {
-    Wire.beginTransmission(device);     // start transmission to device 
-    Wire.write(address);                // send register address
-    Wire.write(val);                    // send value to write
-    Wire.endTransmission();             // end transmission
-}
-
-//reads num bytes starting from address register on device in to buff array
-void readFrom(int device, byte address, int num, byte buff[]) {
-    Wire.beginTransmission(device);     // start transmission to device 
-    Wire.write(address);                // sends address to read from
-    Wire.endTransmission();             // end transmission
-    Wire.beginTransmission(device);     // start transmission to device
-    Wire.requestFrom(device, num);      // request 6 bytes from device
-    int i = 0;
-    while(Wire.available())             // device may send less than requested (abnormal)
-    { 
-        buff[i] = Wire.read();          // receive a byte
-        i++;
-    }
-    Wire.endTransmission();             // end transmission
-}
-
-//calculate the Roll&Pitch
-void RP_calculate(){
-    double x_Buff = float(x);
-    double y_Buff = float(y);
-    double z_Buff = float(z);
-    roll = atan2(y_Buff, z_Buff)*57.3;
-    pitch = atan2(-x_Buff, sqrt(y_Buff*y_Buff+z_Buff*z_Buff))*57.3;
-}
-
-void readGyroscopeSensor()
-{
-    // readFrom(DEVICE, regAddress, TO_READ, buff); //read the acceleration data from the ADXL345
-    //                                              //each axis reading comes in 10 bit resolution, ie 2 bytes.  Least Significat Byte first!!
-    //                                              //thus we are converting both bytes in to one int
-    // x = (((int)buff[1]) << 8) | buff[0];
-    // y = (((int)buff[3]) << 8) | buff[2];
-    // z = (((int)buff[5]) << 8) | buff[4];
-
-    // RP_calculate();
-
-    // data[apJ2xDirection] = map(x, -300, 300, 1023, 0);
-    // data[apJ1ySpeed] = map(y, -300, 300, 0, 1023);
-}
-
 void readButtons()
 {
     if (digitalRead(APin) == LOW)
@@ -193,7 +118,6 @@ void readButtons()
         digitalWrite(led2Pin, HIGH);
         digitalWrite(led3Pin, LOW);
         tJoysticks.enableIfNot();
-        tGyroscope.disable();
     }
     if (digitalRead(BPin) == LOW)
     {
@@ -201,7 +125,6 @@ void readButtons()
         digitalWrite(led2Pin, LOW);
         digitalWrite(led3Pin, HIGH);
         tJoysticks.enableIfNot();
-        tGyroscope.enableIfNot();
     }
     if (digitalRead(CPin) == LOW)
     {
@@ -209,7 +132,6 @@ void readButtons()
         digitalWrite(led2Pin, HIGH);
         digitalWrite(led3Pin, HIGH);
         tJoysticks.disable();
-        tGyroscope.disable();
     }
     if (digitalRead(DPin) == LOW)
     {
@@ -217,17 +139,7 @@ void readButtons()
         digitalWrite(led2Pin, LOW);
         digitalWrite(led3Pin, LOW);
         tJoysticks.disable();
-        tGyroscope.disable();
     }
-    if (digitalRead(ShiftLPin) == LOW)
-    {
-        tGyroscope.disable();
-    }
-    if (digitalRead(ShiftRPin) == LOW)
-    {
-        tGyroscope.disable();
-    }
-
     data[apMode] = mode;
 }
 
@@ -236,53 +148,68 @@ void readJoysticks()
     // only write values for direction and speed in data structure if in joystick mode
     if (mode == 1)
     {
-        // put the values of rocker, switch and potentiometer into the array
-        data[apJ1ySpeed] = analogRead(pinJ1ySpeed);
-        data[apJ2xDirection] = 1023 - analogRead(pinJ2xDirection);
+        // put the values of the pedals and the steering wheel into the array
+        int speedTemp = analogRead(pinJ1ySpeed);
+        if (speedTemp > 460)
+        {   // accelerate
+            data[apSpeed] = (int)constrain(map(speedTemp, 465, 890, 512, 1023), 512, 1023);
+        }
+        else
+        {   // brake
+            data[apSpeed] = (int)constrain(map(speedTemp, 320, 455, 0, 511), 0, 511);
+        }
 
-        // calibration values of the resistors
-        data[apJ1fine] = analogRead(R1Pin);
-        data[apJ2fine] = analogRead(R6Pin);
+        int directionTemp = analogRead(pinJ2xDirection);
+        if (directionTemp > 450)
+        {   // turn left
+            data[apDirection] = (int)constrain(map(directionTemp, 460, 830, 512, 1023), 512, 1023);
+        }
+        else
+        {   // turn right
+            data[apDirection] = (int)constrain(map(directionTemp, 140, 440, 0, 511), 0, 511);
+        }
+
+        // data[apJ2xDirection] = directionTemp;
     }
 
-    data[apJ1x] = analogRead(pinJ1x);
-    data[apJ2y] = analogRead(pinJ2y); // buzzer
+    data[apJ1x] = (int)constrain(map(analogRead(pinGearshiftLeft), 380, 765, 0, 1023), 0, 1023);
+    data[apJ2y] = (int)constrain(map(analogRead(pinGearshiftRight), 345, 710, 0, 1023), 0, 1023); // buzzer
 }
 
 void comRF()
 {
-    // if (mode < 3 || automatic)
-    // {
-    //     // if manual mode, stop listening to incoming signals
-    //     radio.stopListening();
-    //     // send array data. If the sending succeeds
-    //     if (radio.write(data, sizeof(data)))
-    //         digitalWrite(led1Pin, HIGH);
+    if (mode < 3 || automatic)
+    {
+        // if manual mode, stop listening to incoming signals
+        radio.stopListening();
+        // send array data. If the sending succeeds
+        if (radio.write(data, sizeof(data)))
+            digitalWrite(led1Pin, HIGH);
 
-    //     // delay for a period of time, then turn off the signal LED for next sending
-    //     delay(2);
-    //     digitalWrite(led1Pin, LOW);
+        // delay for a period of time, then turn off the signal LED for next sending
+        delay(2);
+        digitalWrite(led1Pin, LOW);
 
-    //     // flag for sending the change to automatic mode at least once, before stopping to sending data
-    //     automatic = mode < 3;
-    // }
-    // else if (mode > 2)
-    // {
-    //     // if automatic mode, start listening
-    //     radio.startListening(); // start monitoring
-    //     if (radio.available())
-    //     { // if receive the data
-    //         while (radio.available())
-    //         {                                   // read all the data
-    //             radio.read(data, sizeof(data)); // read data
-    //         }
-    //         digitalWrite(led1Pin, HIGH);
-    //     }
-    //     else
-    //     {
-    //         digitalWrite(led1Pin, LOW);
-    //     }
-    // }
+        // flag for sending the change to automatic mode at least once, before stopping to sending data
+        automatic = mode < 3;
+    }
+    else if (mode > 2)
+    {
+        // if automatic mode, start listening
+        radio.startListening(); // start monitoring
+        if (radio.available())
+        { // if receive the data
+            while (radio.available())
+            {                                   // read all the data
+                radio.read(data, sizeof(data)); // read data
+            }
+            digitalWrite(led1Pin, HIGH);
+        }
+        else
+        {
+            digitalWrite(led1Pin, LOW);
+        }
+    }
 }
 
 void sendSerial()
@@ -292,11 +219,11 @@ void sendSerial()
     Serial.print("; J1x: ");
     Serial.print(data[apJ1x]);
     Serial.print("; J1ySpeed: ");
-    Serial.print(data[apJ1ySpeed]);
+    Serial.print(data[apSpeed]);
     Serial.print("; J1fine: ");
     Serial.print(data[apJ1fine]);
     Serial.print("; J2xDirection: ");
-    Serial.print(data[apJ2xDirection]);
+    Serial.print(data[apDirection]);
     Serial.print("; J2y: ");
     Serial.print(data[apJ2y]);
     Serial.print("; J2fine: ");
@@ -338,13 +265,13 @@ void serialControl()
                     if (iComandParamter >= 0 && iComandParamter < 1024)
                     {
                         data[apMode] = 0;
-                        data[apJ1ySpeed] = round(iComandParamter);
+                        data[apSpeed] = round(iComandParamter);
                     }
                 }
                 break;
             case 's':
                 // read current speed
-                Serial.println("Speed: " + String(data[apJ1ySpeed]));
+                Serial.println("Speed: " + String(data[apSpeed]));
                 break;
             case 'm': // get current mode
                 Serial.println("Mode: " + String(mode));
@@ -356,20 +283,16 @@ void serialControl()
                     {
                     case 0:
                         tJoysticks.disable();
-                        tGyroscope.disable();
                         break;
                     case 1:
                         tJoysticks.enableIfNot();
-                        tGyroscope.disable();
                         break;
                     case 2:
                         tJoysticks.enableIfNot();
-                        tGyroscope.enableIfNot();
                         break;
                     case 3:
                     case 4:
                         tJoysticks.disable();
-                        tGyroscope.disable();
                         break;
                     }
 
@@ -395,13 +318,13 @@ void serialControl()
                     if (iComandParamter >= 0 && iComandParamter < 1024)
                     {
                         data[apMode] = 0;
-                        data[apJ2xDirection] = round(iComandParamter);
+                        data[apDirection] = round(iComandParamter);
                     }
                 }
                 break;
             case 'r':
                 // read current direction
-                Serial.println("Direction: " + String(data[apJ2xDirection]));
+                Serial.println("Direction: " + String(data[apDirection]));
                 break;
             case 'L':
             case 'l':
@@ -485,15 +408,7 @@ void setup()
     pinMode(CPin, INPUT_PULLUP); // set CPin to output mode
     pinMode(DPin, INPUT_PULLUP); // set DPin to output mode
 
-    pinMode(ShiftLPin, INPUT_PULLUP); // set ShiftLPin to output mode
-    pinMode(ShiftRPin, INPUT_PULLUP); // set ShiftRPin to output mode
-
-    // //Turning on the ADXL345
-    // writeTo(DEVICE, 0x2D, 0);
-    // writeTo(DEVICE, 0x2D, 16);
-    // writeTo(DEVICE, 0x2D, 8);
-
-    Serial.println("Remote booted");
+    Serial.println("Steering wheel booted");
 }
 
 void loop()
